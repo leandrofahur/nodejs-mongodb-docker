593c578a2d27add9cd4e5a26c6b58cdbc80f4eb5
Leandro <fahurleandro@gmail.com>
2021-05-28 14:18:03 -0700

    update

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.get("/all", async (req, res) => {
 });
 
 const PORT = process.env.PORT | 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server up and running on port ${PORT}`);
 });
