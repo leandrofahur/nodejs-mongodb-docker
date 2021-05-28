1a6889469b3ffe571ef2b37c2df131f295453333
Leandro <fahurleandro@gmail.com>
2021-05-27 19:09:45 -0700

    hot reload not working

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose
+  .connect("mongodb://mongo:27017/docker-nodejs-mongodb", {
+    useNewUrlParser: true,
+  })
+  .then(console.log("MongoDB connected!"))
+  .catch((err) => console.error(err));
+
 app.get("/", (req, res) => {
-  console.log("Hello World");
+  res.status(200).json({
+    status: 200,
+    message: "Hello from express in docker-compose!",
+  });
 });
 
 const PORT = process.env.PORT | 5000;
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
+  console.log(`Server up and running on port ${PORT}`);
 });
